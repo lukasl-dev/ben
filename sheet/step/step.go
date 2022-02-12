42ab954f3a3bfd64cf15625ef417c5f0f8d33b96
lukasl-dev <git@lukasl.dev>
2022-02-12 10:58:07 +0100

    feat(step): add Step.Validate()

--- sheet/step/step.go
+++ sheet/step/step.go
@@ -1,9 +1,21 @@
 package step
 
+import (
+	"errors"
+)
+
 // Step represents a general step in a configuration file. Only one of the
 // embedded fields can be set.
 type Step struct {
 	Base
 	*Command
 	*Copy
+}
+
+// Validate validates s.
+func (s Step) Validate() error {
+	if s.Command != nil && s.Copy != nil {
+		return errors.New("step: a step cannot inherit multiple tasks")
+	}
+	return nil
 }
