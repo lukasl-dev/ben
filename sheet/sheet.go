a69e707d289c243b4f0c06aacd818b81f7effe85
lukasl-dev <git@lukasl.dev>
2022-02-12 01:25:09 +0100

    feat(sheet): add Load()

--- sheet/sheet.go
+++ sheet/sheet.go
@@ -1,6 +1,13 @@
 package sheet
 
-import "github.com/lukasl-dev/ben/sheet/job"
+import (
+	"github.com/ghodss/yaml"
+	"github.com/lukasl-dev/ben/sheet/job"
+	"io"
+	"net/http"
+	"net/url"
+	"os"
+)
 
 // Sheet is a collection of jobs. It represents the content of a configuration
 // file.
@@ -14,4 +21,41 @@ type Sheet struct {
 	// Jobs is a map of jobs that can be executed on the sheet. The key is the
 	// unique name of a job.
 	Jobs map[string]job.Job `json:"jobs,omitempty"`
+}
+
+// Load loads a sheet from a file or from a URL.
+func Load(pathOrURL string) (*Sheet, error) {
+	u, err := url.ParseRequestURI(pathOrURL)
+	if err == nil || u.Scheme == "http" || u.Scheme == "https" {
+		return read(pathOrURL)
+	}
+	return fetch(pathOrURL)
+}
+
+// read unmarshals a sheet from a file that is located at the given local path.
+func read(path string) (*Sheet, error) {
+	content, err := os.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+
+	var sheet *Sheet
+	return sheet, yaml.Unmarshal(content, &sheet)
+}
+
+// fetch unmarshals a sheet from the response body of the HTTP request that is
+// sent to the given url.
+func fetch(url string) (*Sheet, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	var sheet *Sheet
+	return sheet, yaml.Unmarshal(body, &sheet)
 }
