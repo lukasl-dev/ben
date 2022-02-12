206ca6e110142685fb9e37fde4ca97077f3d0a77
lukasl-dev <git@lukasl.dev>
2022-02-12 01:05:34 +0100

    feat(job): add Job

---
+++ sheet/job/job.go
@@ -0,0 +1,15 @@
+package job
+
+import "github.com/lukasl-dev/ben/sheet/step"
+
+// Job is a collection of steps that can be executed by a runner.
+type Job struct {
+	// Name is the display name of a job.
+	Name string `json:"name,omitempty"`
+
+	// State is the initial state of a job.
+	State State `json:"state,omitempty"`
+
+	// Steps is a slice of steps that can be executed.
+	Steps []step.Step `json:"steps,omitempty"`
+}
