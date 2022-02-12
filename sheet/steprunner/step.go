5f7ed6afa316dea271fd0df1f0168a30a171a1b4
lukasl-dev <git@lukasl.dev>
2022-02-12 11:19:48 +0100

    feat(steprunner): add Step()

---
+++ sheet/steprunner/step.go
@@ -0,0 +1,22 @@
+package steprunner
+
+import (
+	"fmt"
+	"github.com/lukasl-dev/ben/sheet/step"
+)
+
+// StepOptions holds optional parameter for Step().
+type StepOptions struct {
+	// Command are the CommandOptions to use.
+	Command CommandOptions
+}
+
+// Step runs a step according to its type.
+func Step(step step.Step, options StepOptions) error {
+	switch {
+	case step.Command != nil:
+		return Command(step.Base, *step.Command, options.Command)
+	default:
+		return fmt.Errorf("step: %s: unsupported or invalid task specified", step.Base.Name)
+	}
+}
