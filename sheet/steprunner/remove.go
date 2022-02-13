e867f7d98bbfe53f9e59715558babec78b324d18
lukasl-dev <git@lukasl.dev>
2022-02-13 14:11:08 +0100

    fix(remove steprunner): compile error

--- sheet/steprunner/remove.go
+++ sheet/steprunner/remove.go
@@ -1,15 +1,23 @@
 package steprunner
 
 import (
-	"fmt"
-	"github.com/lukasl-dev/ben/sheet/step"
 	"os"
+
+	"github.com/lukasl-dev/ben/internal"
+	"github.com/lukasl-dev/ben/sheet/step"
 )
 
 // Remove runs a remove step.
-func Remove(base step.Base, remove step.Remove) error {
-	if err := os.Remove(remove.Remove); err != nil {
-		return fmt.Errorf("step: %s: %w", base.Name, err)
+func Remove(st step.Step) error {
+	if err := os.Remove(*st.Remove); err != nil {
+		return &internal.Error{
+			Prefix: "step",
+			Origin: st.Name,
+			Err:    err,
+			Suggestions: []string{
+				"Check if the file or directory exists",
+			},
+		}
 	}
 	return nil
 }
