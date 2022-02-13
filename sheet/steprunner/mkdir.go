8a308bdca8c638556b8a6d591541892888cabecd
lukasl-dev <git@lukasl.dev>
2022-02-13 14:10:16 +0100

    fix(mkdir steprunner): compile error

--- sheet/steprunner/mkdir.go
+++ sheet/steprunner/mkdir.go
@@ -1,15 +1,23 @@
 package steprunner
 
 import (
-	"fmt"
-	"github.com/lukasl-dev/ben/sheet/step"
 	"os"
+
+	"github.com/lukasl-dev/ben/internal"
+	"github.com/lukasl-dev/ben/sheet/step"
 )
 
 // Mkdir runs a mkdir step.
-func Mkdir(base step.Base, mkdir step.Mkdir) error {
-	if err := os.MkdirAll(mkdir.Mkdir, os.ModeDir); err != nil {
-		return fmt.Errorf("step: %s: %w", base.Name, err)
+func Mkdir(st step.Step) error {
+	if err := os.MkdirAll(*st.Mkdir, os.ModeDir); err != nil {
+		return &internal.Error{
+			Prefix: "step",
+			Origin: st.Name,
+			Err:    err,
+			Suggestions: []string{
+				"Check if the path is valid",
+			},
+		}
 	}
 	return nil
 }
