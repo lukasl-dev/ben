3fe22a3dce1b0c0bdcb56ac31879d6eb5605df07
lukasl-dev <git@lukasl.dev>
2022-02-13 14:19:51 +0100

    fix(steprunner): compiler errors

--- sheet/steprunner/step.go
+++ sheet/steprunner/step.go
@@ -1,27 +1,38 @@
 package steprunner
 
 import (
+	"errors"
 	"fmt"
+	"strings"
+
+	"github.com/lukasl-dev/ben/internal"
 	"github.com/lukasl-dev/ben/sheet"
 	"github.com/lukasl-dev/ben/sheet/step"
 )
 
 // Step runs a step according to its type.
-func Step(sheet sheet.Sheet, step step.Step) error {
+func Step(sh sheet.Sheet, st step.Step) error {
 	switch {
-	case step.Clean != nil:
-		return Clean(step.Base, *step.Clean)
-	case step.Command != nil:
-		return Command(sheet, step.Base, *step.Command)
-	case step.Copy != nil:
-		return Copy(step.Base, *step.Copy)
-	case step.Mkdir != nil:
-		return Mkdir(step.Base, *step.Mkdir)
-	case step.Remove != nil:
-		return Remove(step.Base, *step.Remove)
-	case step.Rename != nil:
-		return Rename(step.Base, *step.Rename)
+	case st.Clean != nil:
+		return Clean(st)
+	case st.Command != nil:
+		return Command(sh, st)
+	case st.Copy != nil:
+		return Copy(st)
+	case st.Mkdir != nil:
+		return Mkdir(st)
+	case st.Remove != nil:
+		return Remove(st)
+	case st.Rename != nil:
+		return Rename(st)
 	default:
-		return fmt.Errorf("step: %s: unsupported or invalid task specified", step.Base.Name)
+		return &internal.Error{
+			Prefix: "step",
+			Origin: st.Name,
+			Err:    errors.New("unsupported or invalid task specified"),
+			Suggestions: []string{
+				fmt.Sprintf("Check if the step has one of the following fields: %s", strings.Join(step.StepFields(), ", ")),
+			},
+		}
 	}
 }
