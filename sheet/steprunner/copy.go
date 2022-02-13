b661191308bcc57a9722d801fca058bf4ea07360
lukasl-dev <git@lukasl.dev>
2022-02-13 14:09:10 +0100

    fix(copy steprunner): compile error

--- sheet/steprunner/copy.go
+++ sheet/steprunner/copy.go
@@ -1,15 +1,23 @@
 package steprunner
 
 import (
-	"fmt"
+	"github.com/lukasl-dev/ben/internal"
 	"github.com/lukasl-dev/ben/sheet/step"
 	cp "github.com/otiai10/copy"
 )
 
 // Copy runs a copy step.
-func Copy(base step.Base, copy step.Copy) error {
-	if err := cp.Copy(copy.Copy.From, copy.Copy.To); err != nil {
-		return fmt.Errorf("step: %s: %w", base.Name, err)
+func Copy(st step.Step) error {
+	err := cp.Copy(st.Copy.From, st.Copy.To)
+	if err != nil {
+		return &internal.Error{
+			Prefix: "step",
+			Origin: st.Name,
+			Err:    err,
+			Suggestions: []string{
+				"Check if the source and destination paths are valid",
+			},
+		}
 	}
 	return nil
 }
