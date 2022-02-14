8e820bef0e881f7082fac7aee6d20114b54509a6
lukasl-dev <git@lukasl.dev>
2022-02-14 09:46:27 +0100

    refactor: migrate to viper for sheet loading

--- go.mod
+++ go.mod
@@ -6,20 +6,30 @@ require (
 	github.com/AlecAivazis/survey/v2 v2.3.2
 	github.com/briandowns/spinner v1.18.1
 	github.com/fatih/color v1.13.0
-	github.com/ghodss/yaml v1.0.0
+	github.com/mitchellh/mapstructure v1.4.3
 	github.com/otiai10/copy v1.7.0
 	github.com/spf13/cobra v1.3.0
 	github.com/spf13/pflag v1.0.5
+	github.com/spf13/viper v1.10.1
 )
 
 require (
+	github.com/fsnotify/fsnotify v1.5.1 // indirect
+	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/inconshreveable/mousetrap v1.0.0 // indirect
 	github.com/kballard/go-shellquote v0.0.0-20180428030007-95032a82bc51 // indirect
+	github.com/magiconair/properties v1.8.5 // indirect
 	github.com/mattn/go-colorable v0.1.12 // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect
 	github.com/mgutz/ansi v0.0.0-20200706080929-d51e80ef957d // indirect
+	github.com/pelletier/go-toml v1.9.4 // indirect
+	github.com/spf13/afero v1.8.1 // indirect
+	github.com/spf13/cast v1.4.1 // indirect
+	github.com/spf13/jwalterweatherman v1.1.0 // indirect
+	github.com/subosito/gotenv v1.2.0 // indirect
 	golang.org/x/sys v0.0.0-20220209214540-3681064d5158 // indirect
 	golang.org/x/term v0.0.0-20210927222741-03fcf44c2211 // indirect
 	golang.org/x/text v0.3.7 // indirect
+	gopkg.in/ini.v1 v1.66.4 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
--- sheet/sheet.go
+++ sheet/sheet.go
@@ -1,8 +1,10 @@
 package sheet
 
 import (
-	"github.com/ghodss/yaml"
+	"bytes"
 	"github.com/lukasl-dev/ben/sheet/job"
+	"github.com/mitchellh/mapstructure"
+	"github.com/spf13/viper"
 	"io"
 	"net/http"
 	"net/url"
@@ -40,6 +42,25 @@ func (s Sheet) Validate() error {
 
 // Load loads a sheet from a file or from a URL.
 func Load(pathOrURL string) (*Sheet, error) {
+	r, err := reader(pathOrURL)
+	if err != nil {
+		return nil, err
+	}
+
+	v := viper.New()
+	v.SetConfigFile(pathOrURL)
+	if err := v.ReadConfig(r); err != nil {
+		return nil, err
+	}
+
+	var s *Sheet
+	return s, v.Unmarshal(&s, func(config *mapstructure.DecoderConfig) {
+		config.TagName = "json"
+	})
+}
+
+// reader returns a reader for the given path or URL.
+func reader(pathOrURL string) (io.Reader, error) {
 	u, err := url.ParseRequestURI(pathOrURL)
 	if err == nil && (u.Scheme == "http" || u.Scheme == "https") {
 		return fetch(pathOrURL)
@@ -49,28 +70,19 @@ func Load(pathOrURL string) (*Sheet, error) {
 
 // fetch unmarshals a sheet from the response body of the HTTP request that is
 // sent to the given url.
-func fetch(url string) (*Sheet, error) {
+func fetch(url string) (io.Reader, error) {
 	resp, err := http.Get(url)
 	if err != nil {
 		return nil, err
 	}
-
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		return nil, err
-	}
-
-	var sheet *Sheet
-	return sheet, yaml.Unmarshal(body, &sheet)
+	return resp.Body, nil
 }
 
 // read unmarshals a sheet from a file that is located at the given local path.
-func read(path string) (*Sheet, error) {
+func read(path string) (io.Reader, error) {
 	content, err := os.ReadFile(path)
 	if err != nil {
 		return nil, err
 	}
-
-	var sheet *Sheet
-	return sheet, yaml.Unmarshal(content, &sheet)
+	return bytes.NewReader(content), nil
 }
