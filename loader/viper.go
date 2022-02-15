7ae1645115ea4bf59110eec07f38dcbf26824a20
lukasl-dev <git@lukasl.dev>
2022-02-15 19:56:23 +0100

    refactor(viper loader): set tag name to `json`

--- loader/viper.go
+++ loader/viper.go
@@ -2,6 +2,7 @@ package loader
 
 import (
 	"github.com/lukasl-dev/ben/sheet"
+	"github.com/mitchellh/mapstructure"
 	"github.com/spf13/viper"
 	"io"
 )
@@ -15,5 +16,11 @@ func loadViper(uri string, r io.Reader, opts Options) (*sheet.Sheet, error) {
 	}
 
 	var s sheet.Sheet
-	return &s, v.Unmarshal(&s)
+	return &s, v.Unmarshal(&s, configureViperDecoder)
+}
+
+// configureViperDecoder configures the mapstructure decoder config that is used
+// by viper.
+func configureViperDecoder(cfg *mapstructure.DecoderConfig) {
+	cfg.TagName = "json"
 }
