8ac35d88e0ef6c291f5e18bffa4a359656529dd9
lukasl-dev <git@lukasl.dev>
2022-02-16 18:59:28 +0100

    feat(sheet requirements): add Requirements.Envs

--- sheet/requirement/requirements.go
+++ sheet/requirement/requirements.go
@@ -3,6 +3,9 @@ package requirement
 // Requirements contains required specifications that must be met to run a
 // sheet.
 type Requirements struct {
+	// Envs is a slice of environmental variables that are required.
+	Envs []Env `json:"envs,omitempty" hcl:"env,block"`
+
 	// Paths is a slice of file or directory paths that are required.
 	Paths []Path `json:"paths,omitempty" hcl:"path,block"`
 
